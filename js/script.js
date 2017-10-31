33d95213cbab905773eeaffe1e393ca10e44b821
Lucky <>
2017-10-31 11:18:23 +0800

    第4个DEMO提交

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
+
+let ArrayCom = React.createClass({
+    render:function () {
+        return <ul>
+            {
+                React.Children.map(this.props.children,function (val) {
+                    return <li>this is {val}</li>
+                })
+            }
+        </ul>
+    }
+});
 ReactDOM.render(
-    <h1>haha</h1>,
+    <ArrayCom>
+        <span>这是第一条</span>
+        <span>这是第2条</span>
+    </ArrayCom>,
     document.getElementById('example')
 )
