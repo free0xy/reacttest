6f18991f829fde4f34d83c105e8066713c4c0792
Lucky <>
2017-10-31 17:34:33 +0800

    up

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 
 let ArrayCom = React.createClass({
+    getDefaultProps:function () {
+        return {
+            title:'这是默认'
+        }
+    },
+    propTypes:{
+        title:React.PropTypes.string.isRequired
+    },
     render:function () {
         return <ul>
             {
@@ -10,10 +18,39 @@ let ArrayCom = React.createClass({
         </ul>
     }
 });
+var a='2'
 ReactDOM.render(
-    <ArrayCom>
+    <ArrayCom title={a}>
         <span>这是第一条</span>
         <span>这是第2条</span>
     </ArrayCom>,
+    document.getElementById('example')
+)
+
+var XyCom = React.createClass({
+    getDefaultProps:function () {
+        return {
+            tit:'default'
+        }
+    },
+    propTypes:{
+        tit:React.PropTypes.string.isRequired
+    },
+    handleClick:function () {
+        this.refs.haha.focus();
+        this.refs.haha.value='haha'
+    },
+    render:function () {
+        return (
+            <div>
+                <input type="text" ref="haha" placeholder={this.props.tit} />
+                    <button onClick={this.handleClick}>点击</button>
+            </div>
+        )
+    }
+});
+
+ReactDOM.render(
+    <XyCom tit="明天"/>,
     document.getElementById('example')
 )
